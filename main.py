1c4f20f35453ece92f08cd909e038f66981ad922
Kenjicci <Cincokencstd1@gmail.com>
2023-10-19 17:14:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#push dito
+class Customer():
+  def __init__(self, idnumber, username, password, email, address):
+    #try to inherit the details from the parent class
+    self.idnumber = idnumber
+    self.username = username
+    self.password = password
+    self.email = email
+    self.address = address
+  
+  def login(self):
+    return("Successfully Log in!")
+  #paste here the next process
+  def buy(self):
+    pass
+  #try to paste the product list here
+  def chat(self):
+    pass 
+  def add_to_cart(self):
+    pass
+  def checkout(self):
+    pass
+  def cancel_order(self):
+    pass
+  def logout(self):
+    pass
