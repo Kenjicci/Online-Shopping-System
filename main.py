dc28737dcd7a7bba16591e29c7f8ea625da8c34e
Kenjicci <Cincokencstd1@gmail.com>
2023-10-21 21:45:25 +0800

    Update main.py
    draft 1 (login admin)

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#push dito
+class Admin():
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+  def login(self):
+    while self.password != self.password:
+      print("Invalid Password")
+      
+
